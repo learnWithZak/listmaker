c5ef8bcb7811314065720728982d70424c6d87b2
learnWithZak <learn.with.zak@gmail.com>
2022-09-15 08:57:31 +0200

    first commit

---
+++ app/src/main/java/com/zak/listmaker/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.zak.listmaker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.zak.listmaker.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
