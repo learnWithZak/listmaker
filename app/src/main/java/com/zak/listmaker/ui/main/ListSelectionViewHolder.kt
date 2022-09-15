818f8a4637532af95a09b88ad4ce80ec5fa45364
learnWithZak <learn.with.zak@gmail.com>
2022-09-15 11:00:19 +0200

    refactor: set up the recyclerView adapter

---
+++ app/src/main/java/com/zak/listmaker/ui/main/ListSelectionViewHolder.kt
@@ -0,0 +1,7 @@
+package com.zak.listmaker.ui.main
+
+import androidx.recyclerview.widget.RecyclerView
+
+class ListSelectionViewHolder(val binding: ListSelectionViewHOlderBinding): RecyclerView.ViewHolder(binding.root) {
+
+}
