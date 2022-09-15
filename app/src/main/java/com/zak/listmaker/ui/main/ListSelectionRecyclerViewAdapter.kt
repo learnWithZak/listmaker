a5a196e909d59fdde2ac31fb5dbb014ff6caead0
learnWithZak <learn.with.zak@gmail.com>
2022-09-15 11:16:56 +0200

    refactor: creating the viewHolder

--- app/src/main/java/com/zak/listmaker/ui/main/ListSelectionRecyclerViewAdapter.kt
+++ app/src/main/java/com/zak/listmaker/ui/main/ListSelectionRecyclerViewAdapter.kt
@@ -1,20 +1,23 @@
 package com.zak.listmaker.ui.main
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.zak.listmaker.databinding.ListSelectionViewHolderBinding
 
 class ListSelectionRecyclerViewAdapter: RecyclerView.Adapter<ListSelectionViewHolder>() {
 
+    val listTitles = arrayOf("Shopping List", "Chores", "Android Tutorials")
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListSelectionViewHolder {
-        TODO("Not yet implemented")
+        val binding = ListSelectionViewHolderBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ListSelectionViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ListSelectionViewHolder, position: Int) {
         TODO("Not yet implemented")
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = listTitles.size
 
 }
--- app/src/main/java/com/zak/listmaker/ui/main/ListSelectionViewHolder.kt
+++ app/src/main/java/com/zak/listmaker/ui/main/ListSelectionViewHolder.kt
@@ -1,7 +1,8 @@
 package com.zak.listmaker.ui.main
 
 import androidx.recyclerview.widget.RecyclerView
+import com.zak.listmaker.databinding.ListSelectionViewHolderBinding
 
-class ListSelectionViewHolder(val binding: ListSelectionViewHOlderBinding): RecyclerView.ViewHolder(binding.root) {
+class ListSelectionViewHolder(val binding: ListSelectionViewHolderBinding): RecyclerView.ViewHolder(binding.root) {
 
 }
