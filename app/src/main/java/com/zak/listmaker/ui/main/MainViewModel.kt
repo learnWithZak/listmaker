aa7572b16138c5574c4f0655883e02a1f62cac29
learnWithZak <learn.with.zak@gmail.com>
2022-09-17 07:00:13 +0200

    refactor: updating the viewModel

--- app/src/main/java/com/zak/listmaker/ui/main/MainViewModel.kt
+++ app/src/main/java/com/zak/listmaker/ui/main/MainViewModel.kt
@@ -1,7 +1,36 @@
 package com.zak.listmaker.ui.main
 
+import android.content.SharedPreferences
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.zak.listmaker.models.TaskList
 
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class MainViewModel(private val sharedPreferences: SharedPreferences) : ViewModel() {
+    private val TAG = MainViewModel::class.java.simpleName
+    lateinit var onListAdded: (() -> Unit)
+
+    val lists: MutableList<TaskList> by lazy {
+        retrieveLists()
+    }
+
+    private fun retrieveLists(): MutableList<TaskList> {
+        val taskLists = ArrayList<TaskList>()
+        val sharedPreferencesContents = sharedPreferences.all
+
+        for (taskList in sharedPreferencesContents) {
+            val itemsHashSet = ArrayList(taskList.value as HashSet<String>)
+            for (element in itemsHashSet) {
+                Log.d(TAG, "element of the sharedPref: $element")
+            }
+            val list = TaskList(taskList.key, itemsHashSet)
+            taskLists.add(list)
+        }
+        return taskLists
+    }
+
+    fun saveList(list: TaskList) {
+        sharedPreferences.edit().putStringSet(list.name, list.tasks.toHashSet()).apply()
+        lists.add(list)
+        onListAdded.invoke()
+    }
 }
