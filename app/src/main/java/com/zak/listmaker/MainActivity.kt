86e40eaf1e0c5c1e88c602b6f0d433bea5d6340b
learnWithZak <learn.with.zak@gmail.com>
2022-09-17 05:07:18 +0200

    refactor: add a dialog

--- app/src/main/java/com/zak/listmaker/MainActivity.kt
+++ app/src/main/java/com/zak/listmaker/MainActivity.kt
@@ -2,17 +2,48 @@ package com.zak.listmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import com.zak.listmaker.databinding.ActivityMainBinding
+import com.zak.listmaker.databinding.FragmentMainBinding
 import com.zak.listmaker.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
         }
+
+        binding.fabButton.setOnClickListener {
+            showCreateListDialog()
+        }
+    }
+
+    private fun showCreateListDialog() {
+        val dialogTitle = getString(R.string.name_of_list)
+        val positiveButtonTitle = getString(R.string.create_list)
+
+        val builder = AlertDialog.Builder(this)
+        val listTitleEditText = EditText(this)
+        listTitleEditText.inputType = InputType.TYPE_CLASS_TEXT
+
+        builder.setTitle(dialogTitle)
+        builder.setView(listTitleEditText)
+
+        builder.setPositiveButton(positiveButtonTitle) { dialog, _ ->
+            dialog.dismiss()
+        }
+
+        builder.create().show()
     }
 }
