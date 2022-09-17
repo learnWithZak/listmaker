5a70bc35c4665f571d9e85930b92509c95a80a74
learnWithZak <learn.with.zak@gmail.com>
2022-09-17 08:40:39 +0200

    refactor: Intents and Parcels

--- app/src/main/java/com/zak/listmaker/models/TaskList.kt
+++ app/src/main/java/com/zak/listmaker/models/TaskList.kt
@@ -1,5 +1,30 @@
 package com.zak.listmaker.models
 
-class TaskList(val name: String, val tasks: ArrayList<String> = arrayListOf()) {
+import android.os.Parcel
+import android.os.Parcelable
+
+class TaskList(val name: String, val tasks: ArrayList<String> = arrayListOf()): Parcelable {
+
+    constructor(parcel: Parcel) : this(
+        parcel.readString()!!,
+        parcel.createStringArrayList()!!
+    )
+
+    override fun describeContents(): Int = 0
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        dest.writeString(name)
+        dest.writeStringList(tasks)
+    }
+
+    companion object CREATOR : Parcelable.Creator<TaskList> {
+        override fun createFromParcel(parcel: Parcel): TaskList {
+            return TaskList(parcel)
+        }
+
+        override fun newArray(size: Int): Array<TaskList?> {
+            return arrayOfNulls(size)
+        }
+    }
 
 }
